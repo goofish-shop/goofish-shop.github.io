0db09b35391bb49c4e70dc51634251b8ac1e1730
ahKevinXy <31835335+ahKevinXy@users.noreply.github.com>
2023-06-14 08:16:15 +0000

    修改logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Goofish shop',
   tagline: '个人闲鱼小铺',
-  favicon: 'img/favicon.ico',
+  favicon: '/img/favicon.ico',
 
   // Set the production url of your site here
   url: 'https://goofish-shop.github.io',
